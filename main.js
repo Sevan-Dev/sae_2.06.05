4ebba2c9396e996235cfa0e24cd73691c063db59
unknown <rothsevan@gmail.com>
2024-05-09 19:44:35 +0200

    Ajout de la page login

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-let hamburger = document.querySelector(".checkbox")
-let menu = document.querySelector(".mobile_menu")
-let active = true;
+document.addEventListener('DOMContentLoaded', function() {
+    let hamburger = document.querySelector(".checkbox");
+    let menu = document.querySelector(".mobile_menu");
+    let active = true;
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
 
-hamburger.addEventListener('click', function() {
-    const state = active ? '40%' : '-100%';
-    menu.style.transform = `translateY(${state})`;
-    active = !active;
+    hamburger.addEventListener('click', function() {
+        const state = active ? '40%' : '-100%';
+        menu.style.transform = `translateY(${state})`;
+        active = !active;
+    });
+
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
+
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
 });
