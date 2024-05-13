1514eef90a5a34a5488d1a21e583f5e53c2280c9
hopeshadows <150423542+hopeshadows@users.noreply.github.com>
2024-05-13 23:36:43 +0200

    Update bdd

--- deptinfo.sql
+++ deptinfo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 10 mai 2024 à 00:58
+-- Généré le : lun. 13 mai 2024 à 23:36
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.1.12
 
@@ -28,11 +28,40 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `details` (
-  `(id_detail` int(50) NOT NULL,
-  `id_utilisateur` int(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `type_detail` varchar(20) NOT NULL,
   `description` varchar(500) NOT NULL
 ) ENGINE=Aria DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `details`
+--
+
+INSERT INTO `details` (`email`, `type_detail`, `description`) VALUES
+('joshua.brochot@gmail.com', 'metier', 'Developer / ADMIN'),
+('joshua.brochot@gmail.com', 'biographie', 'Je suis en BUT info a Metz !'),
+('michel.morgan@gmail.com', 'metier', 'Secretaire '),
+('michel.morgan@gmail.com', 'biographie', 'Le foot !');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `evenements`
+--
+
+CREATE TABLE `evenements` (
+  `titre` varchar(20) NOT NULL,
+  `date` date NOT NULL,
+  `heure` time NOT NULL,
+  `description` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `evenements`
+--
+
+INSERT INTO `evenements` (`titre`, `date`, `heure`, `description`) VALUES
+('Portes ouvertes', '2024-09-18', '10:00:00', 'Les portes ouvertes font leur retour le samedis...');
 
 -- --------------------------------------------------------
 
@@ -73,7 +102,8 @@ CREATE TABLE `utilisateurs` (
 --
 
 INSERT INTO `utilisateurs` (`nom`, `prenom`, `email`, `mdp`, `annee_prom`, `no_type_compte`) VALUES
-('Brochot', 'Joshua', 'joshua.brochot@gmail.com', 'joshua', '2023-09-02', 1);
+('Brochot', 'Joshua', 'joshua.brochot@gmail.com', 'joshua', '2023-09-02', 1),
+('MORGAN', 'Michel', 'michel.morgan@gmail.com', 'momi', NULL, 2);
 
 --
 -- Index pour les tables déchargées
@@ -83,7 +113,13 @@ INSERT INTO `utilisateurs` (`nom`, `prenom`, `email`, `mdp`, `annee_prom`, `no_t
 -- Index pour la table `details`
 --
 ALTER TABLE `details`
-  ADD PRIMARY KEY (`(id_detail`);
+  ADD PRIMARY KEY (`email`,`type_detail`);
+
+--
+-- Index pour la table `evenements`
+--
+ALTER TABLE `evenements`
+  ADD PRIMARY KEY (`titre`,`date`,`heure`);
 
 --
 -- Index pour la table `type_compte`
